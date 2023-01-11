3b261c7d65a0738189eb967cdab52d017f009884
Aigars Strods <aigars.strods@tieto.com>
2023-01-11 21:00:28 +0200

    Validate row, column uniqeness and if all are digits

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Validator
   def initialize(puzzle_string)
     @puzzle_string = puzzle_string
@@ -8,6 +10,27 @@ class Validator
   end
 
   def validate
-    # Your code here
+    @puzzle_integer_array = @puzzle_string.scan(/\d/).map(&:to_i)
+    @matrix = @puzzle_integer_array.each_slice(9).to_a
+
+    sudoku_valid?
+  end
+
+  private
+
+  def sudoku_valid?
+    all_are_digits? && [@matrix, @matrix.transpose].all?(&method(:unique_elements?))
+  end
+
+  def unique_elements?(matrix)
+    matrix.all?(&method(:uniq?))
+  end
+
+  def uniq?(array)
+    array & array == array
+  end
+
+  def all_are_digits?
+    @puzzle_integer_array.all? { |element| [*0..9].include?(element) }
   end
 end
