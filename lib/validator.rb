d7829f0cd29e19f64b87009b876ce24156390c33
Aigars Strods <aigars.strods@tieto.com>
2023-01-12 08:22:13 +0200

    Sudoku validation result output and refactor

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
 class Validator
+  PARSED_ARRAY_SIZE = 81
+
+  private_constant :PARSED_ARRAY_SIZE
+
   def initialize(puzzle_string)
     @puzzle_string = puzzle_string
   end
@@ -10,27 +14,37 @@ class Validator
   end
 
   def validate
-    @puzzle_integer_array = @puzzle_string.scan(/\d/).map(&:to_i)
-    @matrix = @puzzle_integer_array.each_slice(9).to_a
-
-    sudoku_valid?
+    @matrix = @puzzle_string.scan(/\d/).map(&:to_i).each_slice(9).to_a
+    "#{formatted_result.join(' ')}."
   end
 
   private
 
+  def formatted_result
+    result = ['Sudoku is']
+    return result << 'invalid' unless sudoku_valid?
+
+    result << 'valid'
+    result << 'but incomplete' unless complete?
+    result
+  end
+
   def sudoku_valid?
-    all_are_digits? && [@matrix, @matrix.transpose].all?(&method(:unique_elements?))
+    return false unless @matrix.flatten.size == PARSED_ARRAY_SIZE
+
+    [@matrix, @matrix.transpose].all?(&method(:unique_elements?))
+  end
+
+  def complete?
+    @matrix.flatten.none?(&:zero?)
   end
 
   def unique_elements?(matrix)
     matrix.all?(&method(:uniq?))
   end
 
-  def uniq?(array)
+  def uniq?(array_to_compare)
+    array = array_to_compare.reject(&:zero?)
     array & array == array
-  end
-
-  def all_are_digits?
-    @puzzle_integer_array.all? { |element| [*0..9].include?(element) }
   end
 end
