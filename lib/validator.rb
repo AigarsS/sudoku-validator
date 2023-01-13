d17f28e71855cdac285f006071985e9b72ca75c0
Aigars Strods <aigars.strods@tieto.com>
2023-01-13 07:06:54 +0200

    Revert "Refactor validation logic and text output to another class"
    This reverts commit 9c883f622d81a8a76ddd5f7976dd16401c80fcec.

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,9 +1,14 @@
-# frozen_string_literal: true
-
-require_relative 'sudoku_validation_service'
-require_relative 'results_formatter'
-
 class Validator
+  VALID_SUDOKU_SIZE = 81
+
+  RESULT_OUTPUT = {
+    valid: 'Sudoku is valid.',
+    invalid: 'Sudoku is invalid.',
+    incomplete: 'Sudoku is valid but incomplete.'
+  }.freeze
+
+  private_constant :VALID_SUDOKU_SIZE, :RESULT_OUTPUT
+
   def initialize(puzzle_string)
     @puzzle_string = puzzle_string
   end
@@ -14,7 +19,41 @@ class Validator
 
   def validate
     @matrix = @puzzle_string.scan(/\d/).map(&:to_i).each_slice(9).to_a
-    validation_result = SudokuValidationService.validate_sudoku_puzzle(@matrix)
-    ResultsFormatter.formatted_result(validation_result)
+
+    RESULT_OUTPUT[validation_result]
+  end
+
+  private
+
+  def validation_result
+    return :invalid unless sudoku_valid?
+    return :incomplete unless complete?
+
+    :valid
+  end
+
+  def sudoku_valid?
+    return false unless @matrix.flatten.size == VALID_SUDOKU_SIZE
+
+    [@matrix, @matrix.transpose, quadrant_matrix].all?(&method(:unique_all_elements?))
+  end
+
+  def quadrant_matrix
+    @matrix
+      .flatten.each_slice(3).each_slice(3).to_a
+      .transpose.flatten.each_slice(9).to_a
+  end
+
+  def complete?
+    @matrix.flatten.none?(&:zero?)
+  end
+
+  def unique_all_elements?(matrix)
+    matrix.all?(&method(:unique?))
+  end
+
+  def unique?(array_to_compare)
+    array = array_to_compare.reject(&:zero?)
+    array & array == array
   end
 end
